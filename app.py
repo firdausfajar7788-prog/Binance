7ebde9c63c6f844e7f8f34c194cb3ba3104acabb
firdausfajar7788-prog <firdausfajar7788@gmail.com>
2026-05-31 08:00:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 import streamlit as st
 import requests
 
-st.title("Binance Debug")
+st.title("Internet Test")
 
-url = "https://api.binance.com/api/v3/ticker/price"
-
-response = requests.get(
-    url,
-    timeout=10
+r = requests.get(
+    "https://api.coingecko.com/api/v3/ping"
 )
 
-st.write("STATUS")
-st.write(response.status_code)
+st.write(r.status_code)
 
-st.write("TEXT")
-st.code(response.text[:1000])
+st.json(r.json())

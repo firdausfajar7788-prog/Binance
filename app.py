d56323eaf95931d34084a179d83e4046bb41ecda
firdausfajar7788-prog <firdausfajar7788@gmail.com>
2026-05-31 07:57:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
 import streamlit as st
 import requests
-import pandas as pd
 
-st.set_page_config(
-    page_title="Binance Test",
-    layout="wide"
+st.title("Binance Debug")
+
+url = "https://api.binance.com/api/v3/ticker/price"
+
+response = requests.get(
+    url,
+    timeout=10
 )
 
-st.title("🚀 Binance API Test")
+st.write("STATUS")
+st.write(response.status_code)
 
-try:
-
-    url = "https://api.binance.com/api/v3/ticker/price"
-
-    data = requests.get(
-        url,
-        timeout=10
-    ).json()
-
-    df = pd.DataFrame(data)
-
-    st.success("Berhasil ambil data Binance")
-
-    st.write(df.head(20))
-
-except Exception as e:
-
-    st.error(f"Error: {e}")
+st.write("TEXT")
+st.code(response.text[:1000])
